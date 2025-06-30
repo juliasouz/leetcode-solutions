32c0d0652c3fed6c953f25802ac57eb421200cbc
juliasouz <juliasouza1308@outlook.com>
2025-06-30 17:59:31 -0300

    Refactor backspaceCompare method for improved readability and consistency

--- strings/backspace-string-compare.py
+++ strings/backspace-string-compare.py
@@ -1,27 +1,27 @@
 class Solution(object):
     def backspaceCompare(self, s, t):
         p1, p2 = len(s) - 1, len(t) - 1
-    
+        
         while p1 >= 0 or p2 >= 0:
-            if (s[p1] == '#') or (t[p2] == '#'):
+            if (p1 >= 0 and s[p1] == '#') or (p2 >= 0 and t[p2] == '#'):
                 if p1 >= 0 and s[p1] == '#':
-                    back_count = 2
-                    while back_count > 0:
+                    backCount = 2
+                    while backCount > 0:
                         p1 -= 1
-                        back_count -= 1
+                        backCount -= 1
                         if p1 >= 0 and s[p1] == '#':
-                            back_count += 2
-                if  t[p2] == '#':
-                    back_count = 2
-                    while back_count > 0:
+                            backCount += 2
+                if p2 >= 0 and t[p2] == '#':
+                    backCount = 2
+                    while backCount > 0:
                         p2 -= 1
-                        back_count -= 1
+                        backCount -= 1
                         if p2 >= 0 and t[p2] == '#':
-                            back_count += 2
+                            backCount += 2
             else:
-                if s[p1] != t[p2]:
+                if (p1 >= 0 and p2 >= 0 and s[p1] != t[p2]) or ((p1 >= 0) != (p2 >= 0)):
                     return False
-                else:
-                    p1 -= 1
-                    p2 -= 1
+                p1 -= 1
+                p2 -= 1
+        
         return True
